3d33c24482844f26390d0e83d68603796c971086
Shannon Taylor <shannon.taylor@zoetis.com>
2024-10-12 13:02:05 -0400

    updated js to remove html coding, updated some terminology using ctl H

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Dropdown Example</title>
-</head>
-<body>
-    <input type="text" id="textInput" placeholder="Enter text here">
-    <button onclick="addToDropdown()">Add to Dropdown</button>
-    <select id="dropdown">
-        <option value="">Select an option</option>
-    </select>
-
-    <select id="itemDropdown" onchange="addItemToList()">
-        <option value="Item 1">Item 1</option>
-        <option value="Item 2">Item 2</option>
-    </select>
-
-    <script>
+//I removed the script from the HTML file, it was casuing issues- ST.
         let items = [];
 
         function addToDropdown() {
@@ -33,6 +13,4 @@
                 console.log(items); // Log the array to verify the items
             }
         }
-    </script>
-</body>
-</html>
+    
