ba1316d9db7099e0098beb6bacfa4cb566a5d149
Shannon Taylor <shannon.taylor@zoetis.com>
2024-10-14 08:09:22 -0400

    code option for list  checkboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,29 @@
         }
     */
 
+        /*This is the code from the activities we did in week 4 26- local storage todos:
+        I think this is a good example of how to store the data in local storage. I will use this as a reference to store the data in local storage. -ST.
+        
+        const todoInput = document.querySelector('#todo-text');
+        const todoForm = document.querySelector('#todo-form');
+        const todoList = document.querySelector('#todo-list');
+        const todoCountSpan = document.querySelector('#todo-count');
+
+const todos = [];
+
+//I found this and think it may be what we need for the item checkboxes. 
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+checkbox.id = 'listCheckbox';
+checkbox.name = 'listCheckbox';
+
+//Checkbox label
+const label = document.createElement('label');
+label.hemlFor = 'listCheckbox';
+label.appendChild(document.createTextNode('List Item'));
+
+//Append the checkbox and label to a container element
+const container = document.getElementById('listContainer');
+container.appendChild(checkbox);
+container.appendChild(label);
         
